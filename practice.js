cca267b5f329228f5cf4cadd634a173839b2292b
bournejj <82320254+bournejj@users.noreply.github.com>
2021-04-26 11:51:15 +0200

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,13 @@
+const PI = 3.14;
+PI = 42;
+// Won't work!
+
+
+Quiz
+1. Var can be reassigned and redeclared but let can't be redeclared
+
+2. Var can be reassigned and redeclared but const can't redeclared or reassigned
+
+3. let can be reassigned but const can't redeclared or 
+
+4. Variables are put at the top(hoisted) of the scope they are inside of.Var can be used before it is used.
